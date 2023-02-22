7f93665ce712d9d44505f24895233a06d67abe9a
Jean Will <113138826+Jean-Will@users.noreply.github.com>
2023-02-22 22:16:41 +0000

    first commit

---
+++ validation.js
@@ -0,0 +1,15 @@
+function validarEmail(){
+    var email = document.getElementById("inputEmail").value;
+
+    if(email===" "){
+        alert("Preencher campo obrigatorio!")
+
+    }else if(email.indexOf("@")== -1 ){
+            alert("email invalido")
+        }
+    }
+
+
+console.log(validarEmail);
+
+alert("teste");
